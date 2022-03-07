b6fb60138312ec2692cfa1bb0453d7373bb55db7
Miguel Ângelo Machado Martins <56049132+MiguelAMM42@users.noreply.github.com>
2022-03-07 00:13:46 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var todayRaw = new Date();
+var todayYY = todayRaw.getFullYear();
+var todayMM = todayRaw.getMonth();
+var todayDD = todayRaw.getDate();
+var today = new Date(todayYY, todayMM, todayDD);
+var video = new Date(2021, 09, 06); 
+
+var difference = Math.round((today - video)/(1000*60*60*24));
+document.querySelector('#days').innerHTML = difference;
