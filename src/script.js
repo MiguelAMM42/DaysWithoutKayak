3e5e41b2916ef39ddfd3a4c77cf7517846280e9f
MiguelAMM42 <a93280@alunos.uminho.pt>
2023-01-27 14:19:17 +0000

    Modal for the days content

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-function days() {
+
   var todayRaw = new Date();
   var todayYY = todayRaw.getFullYear();
   var todayMM = todayRaw.getMonth();
@@ -7,6 +7,37 @@ function days() {
   var video = new Date(2021, 09, 06); 
 
   var difference = Math.round((today - video)/(1000*60*60*24));
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  var btn = document.getElementById("daysButton");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  var days = "Days without a kayak: " + difference;
+
+  console.log(days);
+
+  var par = document.getElementById("daysTxt");
+  par.innerHTML = days;
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
     
-  alert("Days without a kayak: " + difference);  
-}
+
