04dc35f24a80b75edc016a68a9d6046ec895165d
MiguelAMM42 <a93280@alunos.uminho.pt>
2023-08-29 17:25:29 +0100

    More small details and dates and Leaflet base

---
+++ src/maps.js
@@ -0,0 +1,9 @@
+var map = L.map('map-test').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© <a href="https://www.openstreetmap.org">OpenStreetMap</a>'
+}).addTo(map);
+
+
+    
--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@
   var todayMM = todayRaw.getMonth();
   var todayDD = todayRaw.getDate();
   var today = new Date(todayYY, todayMM, todayDD);
-  var video = new Date(2021, 09, 06); 
+  var video = new Date(2021, 9, 6); 
 
   var difference = Math.round((today - video)/(1000*60*60*24));
 
